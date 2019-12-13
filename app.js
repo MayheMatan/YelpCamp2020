ba1d1c452726346906f851a5ebe3e795301e2740
Matan Fried <matanfriedleet@hotmail.com>
2019-12-13 16:20:46 +0000

    add dynamic port

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
 	console.log("YelpCamp Server is Running!");
 });
